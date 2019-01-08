00bf60d3ca8602781454861b569ec0dfcda9d085
clare <kirabo646@googlemail.com>
2019-01-08 15:46:33 +0000

    first commit

---
+++ jenkinsTextFile.bash
@@ -0,0 +1,11 @@
+#!bin/bash
+
+cd /home/clare
+sudo apt update
+sudo aapt install openjdk-8-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo apt install jenkins
+systemctl status jenkins
